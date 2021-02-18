d90a4bd2bc87f8960b75f0d89e341611c5b726c8
Vectoryx <leomonta2002@gmail.com>
2021-02-18 22:13:17 +0100

    pt1 aggiunta metodi pr docente

---
+++ TRM-PHP/docente/aggiungi_domanda.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+include "..\\DBConnection.php";
+
+// print_r($_SESSION);
+// echo "<br> <br>"; 
+// print_r($_POST);
+
+$creatore = $_SESSION["user"]["ID"];
+$testo = $_POST["testo"];
+$materia = $_POST["materia"];
+$argomento = $_POST["argomento"];
+$punteggio = $_POST["punteggio"];
+$img_url = "TRM-PHP\\images" . $_FILES["immagine"]["name"];
+$tipo = $_POST["selezione-tipo"];
+
+// tutti questi campi devono esistere, quindi
+if ($argomento and $creatore and $img_url and $materia and $punteggio and $testo and $tipo) {
+
+	$query = "INSERT INTO domande (`testo`, `img_url`, `id_materia`, `punteggio`, `tipo`, `creatore`, `id_argomento`) 
+						VALUES ('$testo', '$img_url', '$materia', $punteggio, $tipo, $creatore, '$argomento');";
+	mysqli_query($conn, $query);
+	move_uploaded_file($_FILES["immagine"]["tmp_name"], "..\\images\\" . $_FILES["immagine"]["name"]);
+}
+// header("Location: questions.php")
